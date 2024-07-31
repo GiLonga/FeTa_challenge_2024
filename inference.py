326828d9abf7b06d6a5e5bd5fe18d4ceb50f97bc
GiLonga <giorgio.longari@mail.polimi.it>
2024-07-31 07:50:11 +0000

    Updating the optimization class.

--- inference.py
+++ inference.py
@@ -9,9 +9,24 @@ from src.modules.lightning_cnn import LitCNN
 from src.utils.optimization import Optimization
 import nibabel as nib
 from torch.utils.data import TensorDataset
+import pandas as pd
+import ants
+import os
+import SimpleITK as sitk
 
 MODEL_PATH = r"/home/ubuntu/giorgio/v311/lightning_logs/brain_model/version_111/checkpoints/epoch=29-step=180.ckpt"
 IMG_PATH = r"/home/ubuntu/giorgio/v311/FeTa_challenge_2024/goundtruth_testBosisio/goundtruth/sub-010_dseg_warped_0.5mm.nii.gz"
+MRI_PATH = r"/home/ubuntu/giorgio/v311/FeTa_challenge_2024/MRI_testBosisio/images/sub-010_brain_warped_0.5mm.nii.gz"
+TRANS_FILE = r"/home/ubuntu/giorgio/v311/FeTa_challenge_2024/0GenericAffine.mat"
+OUT_PATH = r"/home/ubuntu/giorgio/v311/FeTa_challenge_2024"
+
+REGION_DICT = {
+    "LCC": 1,
+    "HV": 2,
+    "bBIP": 3,
+    "sBIP": 4,
+    "TCD": 5,
+    }
 
 def resize(array: np.array,) :
     target_shape = (128, 128, 128)
@@ -24,15 +39,26 @@ def resize(array: np.array,) :
     resized_array = zoom(array, zoom_factors, order=0)  # 'order=0' for nearest-neighbor interpolation
     return resized_array
 
+def inverse_resize(resized_array: np.array, keypoints: np.array, original_shape=(176,224,176)):
+    # Calculate the inverse zoom factors
+    zoom_factors = [o / t for t, o in zip(resized_array.shape, original_shape)]
+
+    # Resize the array back to the original shape using nearest-neighbor interpolation
+    original_array = zoom(resized_array, zoom_factors, order=0)  # 'order=0' for nearest-neighbor interpolation
+
+    transformed_keypoints = np.array([(int(x * zoom_factors[0]), int(y * zoom_factors[1]), int(z * zoom_factors[2])) for x, y, z in keypoints])
+    
+    return original_array, transformed_keypoints
+
+
 def reshape_output(
     y_hat: torch.Tensor,
 ) -> torch.Tensor:
     """
-    Reshape the output tensor of the 5_355 model to the 90 model.
+    Reshape the output tensor (1,30) of the model to the (10,3) model.
 
     Parameters:
-    - y_hat (torch.Tensor): Predicted tensor from the 5_355 model.
-    - patient_idx (int): Index of the patient in the test set, to be reshaped.
+    - y_hat (torch.Tensor): Predicted tensor (1,30) from the model.
 
     Returns:
     - torch.Tensor: output tensor with resized shape.
@@ -43,54 +69,53 @@ def reshape_output(
 
     return y_hat_new
 
-def import_img(IMG_PATH:str,):
+def import_img(IMG_PATH:str, MRI_PATH:str):
+    
     raw_img = nib.load(IMG_PATH)
     my_img = raw_img.get_fdata()
+    raw_MRI = nib.load(MRI_PATH)
+    my_MRI = raw_MRI.get_fdata()
     resized_img = resize(my_img)
+    resized_MRI = resize(my_MRI)
     resized_img=np.expand_dims(resized_img, axis=0)
     resized_img=np.expand_dims(resized_img, axis=0)
-# Verify the result
-    print(resized_img.shape)
-    return resized_img
 
+    return resized_img, resized_MRI
 
+def get_dist(im, region):
+    """
+    Get the distance between the two points of a given region.
+    """
+    x, y, z = np.where(sitk.GetArrayFromImage(im) == REGION_DICT[region])
+    if len(x) == 0:
+        return np.nan
+    p1 = np.array([x[0], y[0], z[0]])
+    p2 = np.array([x[1], y[1], z[1]])
+    ip_res = im.GetSpacing()[0]
+    assert len(x) == 2, f"Region {region} has {len(x)} points"
+    dist = round(np.linalg.norm((p1 - p2) * ip_res), 2)
+    return dist
 
 if __name__ == "__main__":
 
-    resized_img = import_img(IMG_PATH) 
-
-    (brains,
-    y_reg,
-    predict_index,
-    ) = tuple(
-        map(
-            torch.Tensor,
-            (
-                resized_img,
-                [np.ones(30)],
-                1,
-            ),
-        )
-    )
-    pred_dataset = TensorDataset(
-        brains,
-        y_reg,
-        predict_index,
-    )
-
-    dataset = pred_dataset
+    resized_img, resized_MRI = import_img(IMG_PATH, MRI_PATH) 
     lightning_cnn = LitCNN.load_from_checkpoint(checkpoint_path=MODEL_PATH)
-
-    prediction_loader = DataLoader(
-        dataset,
-        num_workers=0,
-    )
-
-    biometry=trainer.predict(lightning_cnn, dataloaders=prediction_loader, return_predictions=True)
+    biometry=lightning_cnn(torch.Tensor(resized_img).cuda())
     biometry=reshape_output(biometry[0])
+    opti= Optimization(resized_img, resized_MRI, biometry,)
+    opti.optimize()
+    biometry=opti.biometry
+    original_img, biometry = inverse_resize(resized_img[0][0], biometry)
 
-
-
-    trainer = Trainer(logger=False)
-
-    print("The results are:",  biometry, "patients")
+    # Dataframe for mapping keypoints to image space
+    biometry_df = pd.DataFrame(biometry, columns=['x','y','z'])
+    # Apply inverse transform using "apply_transforms_to_points"
+    biometryTrans_df = ants.apply_transforms_to_points(3, biometry_df, [TRANS_FILE], whichtoinvert=[True], verbose=False)
+    # Save coordinates of mapped keypoints using .csv file
+    biometryTrans_df.to_csv(os.path.join(OUT_PATH, "keypointsFinal_coords.csv"))    
+    # For each couple of points measure their distance and put in csv file with appropriate label 
+    
+    
+    # Save measures dataframe
+   
+    print( biometry)
--- src/utils/optimization.py
+++ src/utils/optimization.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 from scipy.spatial.distance import euclidean
 from numpy import ones,vstack
 from numpy.linalg import lstsq
+import sys
 
 
 @dataclass
@@ -38,59 +39,155 @@ class Optimization:
 
     def __init__(
         self,
-        patient_mask: int,
+        patient_mask: np.array,
+        patient_MRI: np.array,
         biometry_output: np.array,
     ) -> None:
-        self.brain_mask=patient_mask
-        self.biometry=biometry_output
+        self.brain_mask = patient_mask[0][0]
+        self.brain_MRI = patient_MRI
+        self.biometry = biometry_output
 
-    def find_the_line(self):
-        points = [(self.biometry[5,0],self.biometry[5,1]),(self.biometry[4,0],self.biometry[4,1])] #the necessary biom.
-        x_coords, y_coords = zip(*points) #li zippo
-        A = vstack([x_coords,ones(len(x_coords))]).T
-        self.slope, self.intercept = lstsq(A, y_coords)[0]
-        print("Line Solution is y = {m}x + {c}".format(m = self.slope,c = self.intercept))
+    def find_the_line(self,axis="trs"):
+        if axis=="trs":
+            points = [(self.biometry[self.organ+1,0],self.biometry[self.organ+1,1]),(self.biometry[self.organ,0],self.biometry[self.organ,1])] 
+            x_coords, y_coords = zip(*points) #li zippo
+            A = vstack([x_coords,ones(len(x_coords))]).T
+            self.slope, self.intercept = lstsq(A, y_coords)[0]
+            #print("Line Solution is y = {m}x + {c}".format(m = self.slope,c = self.intercept))
+        elif axis=="sag":
+            points = [(self.biometry[self.organ,2],self.biometry[self.organ,1]),(self.biometry[self.organ+1,2],self.biometry[self.organ+1,1])]
+            x_coords, y_coords = zip(*points) #li zippo
+            A = vstack([x_coords,ones(len(x_coords))]).T
+            self.slope, self.intercept = lstsq(A, y_coords)[0]
+            #print("Line Solution is y = {m}x + {c}".format(m = self.slope,c = self.intercept))
 
-    def abline(self):
+    def segment_LCC(self):
+        organ=0
+        np.set_printoptions(threshold=sys.maxsize)
+        sag_img=self.brain_MRI[int((self.biometry[organ+1,2]+self.biometry[organ,2])/2),:,:]
+        min_val = np.min(sag_img)
+        max_val = np.max(sag_img)
+        # Apply the min-max normalization formula
+        normalized_array = (sag_img - min_val) / (max_val - min_val)
+        #print(normalized_array)
+        boundary=0.6
+        self.LCC_mask = np.where(normalized_array < boundary, 0, 1)
+        while (np.sum(self.LCC_mask)<2500) :
+            boundary-=0.01
+            self.LCC_mask = np.where(normalized_array < boundary, 0, 1)
+        #while (np.sum(self.LCC_mask)>4200) :
+        #    boundary+=0.01
+        #    self.LCC_mask = np.where(normalized_array < boundary, 0, 1)
+        #print(np.sum(self.LCC_mask))
+        #plt.imshow(self.LCC_mask, cmap='gray')
+        #plt.scatter(self.biometry[0,2], self.biometry[0,1], c="red", s=3)
+        #plt.scatter(self.biometry[1,2], self.biometry[1,1],  c="red", s=3)
+    
+    def optimize_LCC(self):
+        #while (self.LCC_mask[int(self.biometry[1,2]), int(self.biometry[1,1])]) == 0 :
+        #        self.biometry[1,1]=+1
+        #plt.imshow(self.LCC_mask, cmap='gray')
+        #plt.scatter(self.biometry[0,2], self.biometry[0,1], c="red", s=3)
+        #plt.scatter(self.biometry[1,2], self.biometry[1,1],  c="red", s=3)
+        print("TO DO")
+
+    def abline(self, axis="trs"):
         """Plot a line from slope and intercept"""
-        plt.imshow(self.brain_mask[:,:,self.biometry[3]], cmap='gray') #To check the index of biometry
-        plt.xlim(0, 128)
-        plt.ylim(0, 128)
-        self.x_vals = range(128)
-        self.y_vals = self.intercept + self.slope * self.x_vals
-        plt.plot( self.y_vals,self.x_vals, '--')
+        if axis=="trs":
+            #plt.imshow(self.brain_mask[:,:,int((self.biometry[self.organ+1,2]+self.biometry[self.organ,2])/2)], cmap='gray') #To check the index of biometry
+            self.x_vals = range(128)
+            self.y_vals = self.intercept + self.slope * self.x_vals
+            #plt.plot( self.y_vals,self.x_vals, '--')
+        elif axis=="sag":
+            #plt.imshow(self.brain_mask[int((self.biometry[self.organ+1,0]+self.biometry[self.organ,0])/2),:,:], cmap='gray') #To check the index of biometry
+            self.x_vals = range(128)
+            self.y_vals = self.intercept + self.slope * self.x_vals
+            #plt.plot( self.x_vals, self.y_vals, '--')
 
-    def filter_points_by_mask(self, mask, label:int):
+    def filter_points_by_mask(self, mask, label:int, axis="trs"):
         """Filter points that fall within the mask."""
         mask_points = []
+        if axis=="sag":
+            dummy=self.x_vals
+            self.x_vals=self.y_vals
+            self.y_vals=dummy
         for point_x, point_y in zip(self.x_vals, self.y_vals) :
             x, y = int(point_x), int(point_y)
             if mask[x, y]==label:  # Note: Mask indices should be in (row, col) format
-                mask_points.append((point_x,point_y))
+                if axis=="sag":
+                    mask_points.append((point_y,point_x))
+                else:
+                    mask_points.append((point_x,point_y))
         self.mask_points=np.array(mask_points)
 
-
     # TO DO try to figure out if points is an array or a list.
-    def find_max_distance_points(self, orig_1, orig_2, points):
-        """Find the two points that maximize the distance between them."""
+    def find_max_distance_points(self, orig_1, orig_2):
+        """
+        
+        Find the two points that maximize the distance between them.
+
+        """
         max_dist = 0
         max_points = (orig_1, orig_2)
-        for i in range(len(points)):
-            for j in range(i + 1, len(points)):
-                dist = euclidean(points[i], points[j])
+        for i in range(len(self.mask_points)):
+            for j in range(i + 1, len(self.mask_points)):
+                dist = euclidean(self.mask_points[i], self.mask_points[j])
                 if dist > max_dist:
                     max_dist = dist
-                    max_points = (points[i], points[j])
+                    max_points = (self.mask_points[i], self.mask_points[j])
         self.max_points=max_points
 
 
-
     def optimize(self):
-        for i in range(4):
-            if  i == 2 :
+        for i in range(5):
+            if i == 1 :
+                self.organ = 2
+                self.find_the_line(axis="sag")
+                self.abline(axis="sag")
+                self.filter_points_by_mask(self.brain_mask[int((self.biometry[self.organ+1,0]+self.biometry[self.organ,0])/2),:,:], 5, axis="sag")
+                Spoint_1=[self.biometry[self.organ,2] , self.biometry[self.organ,1]]
+                Spoint_2=[self.biometry[self.organ+1,2] , self.biometry[self.organ+1,1]]
+                self.find_max_distance_points(Spoint_1,Spoint_2)
+                self.biometry[self.organ+1,2]=self.max_points[0][0]
+                self.biometry[self.organ+1,1]=self.max_points[0][1]
+                self.biometry[self.organ,2]=self.max_points[1][0]
+                self.biometry[self.organ,1]=self.max_points[1][1]
+
+            elif  i == 2 :
+                self.organ=4
                 self.find_the_line()
                 self.abline()
-                # TO DO 69 has to be removed.
-                self.filter_points_by_mask(self.brain_mask[:,:,69], 2)
-                # TO DO 
-                self.find_max_distance_points([22,56],[105,55])
+                self.filter_points_by_mask(self.brain_mask[:,:,int((self.biometry[self.organ+1,2]+self.biometry[self.organ,2])/2)], 2)
+                Spoint_1=[self.biometry[self.organ+1,0] , self.biometry[self.organ+1,1]]
+                Spoint_2=[self.biometry[self.organ,0] , self.biometry[self.organ,1]]
+                self.find_max_distance_points(Spoint_1,Spoint_2)
+                self.biometry[self.organ+1,0]=self.max_points[0][0]
+                self.biometry[self.organ+1,1]=self.max_points[0][1]
+                self.biometry[self.organ,0]=self.max_points[1][0]
+                self.biometry[self.organ,1]=self.max_points[1][1]
+            elif i == 3 :
+                self.organ=6
+                self.find_the_line()
+                self.abline()
+                self.filter_points_by_mask(self.brain_mask[:,:,int((self.biometry[7,2]+self.biometry[6,2])/2)], 1)
+                Spoint_1=[self.biometry[self.organ+1,0] , self.biometry[self.organ+1,1]]
+                Spoint_2=[self.biometry[self.organ,0] , self.biometry[self.organ,1]]
+                self.find_max_distance_points(Spoint_1,Spoint_2)
+                self.biometry[self.organ+1,0]=self.max_points[0][0]
+                self.biometry[self.organ+1,1]=self.max_points[0][1]
+                self.biometry[self.organ,0]=self.max_points[1][0]
+                self.biometry[self.organ,1]=self.max_points[1][1]
+            elif i == 4 :
+                #                                   TO DO: 
+                # #This is optimized from a trasversal POV. It should be done from the Coronal
+                self.organ=8
+                self.find_the_line()
+                self.abline()
+                self.filter_points_by_mask(self.brain_mask[:,:,int((self.biometry[self.organ+1,2]+self.biometry[self.organ,2])/2)], 5)
+                Spoint_1=[self.biometry[self.organ+1,0] , self.biometry[self.organ+1,1]]
+                Spoint_2=[self.biometry[self.organ,0] , self.biometry[self.organ,1]]
+                self.find_max_distance_points(Spoint_1,Spoint_2)
+                self.biometry[self.organ+1,0]=self.max_points[0][0]
+                self.biometry[self.organ+1,1]=self.max_points[0][1]
+                self.biometry[self.organ,0]=self.max_points[1][0]
+                self.biometry[self.organ,1]=self.max_points[1][1]
